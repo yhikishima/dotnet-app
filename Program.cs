d7c88b85768d8fa69d151446e1650f3b02da5b50
yhikishima <lallapalooza1120@gmail.com>
2020-02-02 21:29:36 +0900

    app create

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_aop
+{
+    class Program
+    {
+        static void Main(string[] args)
+    	{
+	    Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, World!"));
+        }
+    }
+}
